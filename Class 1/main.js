7ad802db7326bd477835a7598ace25d4189ee88e
Prabesh <tandukarprabesh@gmail.com>
2024-02-03 00:38:40 +1300

    landing page

--- Class 1/main.js
+++ Class 1/main.js
@@ -1,72 +1,104 @@
-//variables: let, const, var
-//if else
-//for loop
+// //variables: let, const, var
+// //if else
+// //for loop
 
-// for (let i = 0; i < 1001; i++) {
-//   console.log(i);
+// // for (let i = 0; i < 1001; i++) {
+// //   console.log(i);
+// // }
+
+// //Array
+// const personArray = ["ram", "sam", "hari"];
+
+// const ages = [21, 22, 23, 24, 25];
+
+// //print all the even numbers form the ages array
+// for (let i = 0; i < ages.length; i++) {
+//   if (ages[i] % 2 == 0) {
+//     console.log(ages[i]);
+//   }
 // }
 
-//Array
-const personArray = ["ram", "sam", "hari"];
+// //write a program to print the biggest number in an array
+// const big = [100, 4, 7, 8, 9, 111111, 89, 78787878];
+// let biggest = big[0];
+// for (let i = 0; i < big.length; i++) {
+//   if (big[i] > biggest) {
+//     biggest = big[i];
+//   }
+// }
+// console.log(biggest);
 
-const ages = [21, 22, 23, 24, 25];
+// //make two arrays and store the person name in one array and the persons gender in another array , these are two separate array but just connected logically
+// //then print the name of the person whose gender are male
+// const personsArray = ["ram", "sam", "hari", "sita", "rita"];
+// const genderArray = ["male", "male", "male", "female", "female"];
 
-//print all the even numbers form the ages array
-for (let i = 0; i < ages.length; i++) {
-  if (ages[i] % 2 == 0) {
-    console.log(ages[i]);
-  }
+// for (let i = 0; i < personsArray.length; i++) {
+//   if (genderArray[i] == "male") {
+//     console.log(personsArray[i]);
+//   }
+// }
+
+// const user1 = {
+//   firstName: "ram",
+//   gender: "male",
+// };
+
+// console.log(user1["firstName"]);
+
+// const allUsers = [
+//   { firstName: "ram", gender: "male" },
+//   { firstName: "sam", gender: "male" },
+//   { firstName: "sita", gender: "female" },
+// ];
+
+// for (let i = 0; i < allUsers.length; i++) {
+//   if (allUsers[i]["gender"] == "male") {
+//     console.log(allUsers[i]["firstName"]);
+//   }
+// }
+
+// //write a program that reverses all the elements of an array
+// const reverseThis = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let reversed = [];
+// for (let i = reverseThis.length - 1; i >= 0; i--) {
+//   console.log(i);
+//   reversed.push(reverseThis[i]);
+// }
+// console.log(reversed);
+
+// console.log(reverseThis.length);
+
+//function/ callbacks
+//another function is being
+// function sum(num1, num2, fnToCall) {
+//   let result = num1 + num2;
+//   fnToCall(result);
+// }
+
+// function displayResult(data) {
+//   console.log("Result of the sum is : " + data);
+// }
+
+// function displayPassiveResult(data) {
+//   console.log("Sum's result is : " + data);
+// }
+
+// // const ans = sum(1, 2);
+// sum(1, 3, displayPassiveResult);
+
+function calculateArithmetic(a, b, arithmeticFinalFunction) {
+  const ans = arithmeticFinalFunction(a, b);
+  return ans;
 }
 
-//write a program to print the biggest number in an array
-const big = [100, 4, 7, 8, 9, 111111, 89, 78787878];
-let biggest = big[0];
-for (let i = 0; i < big.length; i++) {
-  if (big[i] > biggest) {
-    biggest = big[i];
-  }
-}
-console.log(biggest);
-
-//make two arrays and store the person name in one array and the persons gender in another array , these are two separate array but just connected logically
-//then print the name of the person whose gender are male
-const personsArray = ["ram", "sam", "hari", "sita", "rita"];
-const genderArray = ["male", "male", "male", "female", "female"];
-
-for (let i = 0; i < personsArray.length; i++) {
-  if (genderArray[i] == "male") {
-    console.log(personsArray[i]);
-  }
+function sum(a, b) {
+  return a + b;
 }
 
-const user1 = {
-  firstName: "ram",
-  gender: "male",
-};
-
-console.log(user1["firstName"]);
-
-const allUsers = [
-  { firstName: "ram", gender: "male" },
-  { firstName: "sam", gender: "male" },
-  { firstName: "sita", gender: "female" },
-];
-
-for (let i = 0; i < allUsers.length; i++) {
-  if (allUsers[i]["gender"] == "male") {
-    console.log(allUsers[i]["firstName"]);
-  }
+function sub(a, b) {
+  return a - b;
 }
 
-//write a program that reverses all the elements of an array
-const reverseThis = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let reversed = [];
-for (let i = reverseThis.length - 1; i >= 0; i--) {
-  console.log(i);
-  reversed.push(reverseThis[i]);
-}
-console.log(reversed);
-
-console.log(reverseThis.length);
-
-function 
+const value = calculateArithmetic(1, 2, sum);
+console.log(value);
