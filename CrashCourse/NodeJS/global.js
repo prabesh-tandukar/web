925f50e66595777ff0d0c2c321e7a76803a73ce8
Prabesh <tandukarprabesh@gmail.com>
2024-02-11 01:39:15 +1300

    node crash course

---
+++ CrashCourse/NodeJS/global.js
@@ -0,0 +1,17 @@
+//Global object
+
+// console.log(global);
+
+// global.setTimeout(() => {
+//   console.log("in the timeout");
+//   clearInterval(int);
+// }, 3000);
+// const int = setInterval(() => {
+//   console.log("in the interval");
+// }, 1000);
+
+//gives the current directory of the file
+// console.log(__dirname);
+
+//GIVES THE FILE NAME
+// console.log(__filename);
---
+++ CrashCourse/NodeJS/people.js
@@ -0,0 +1,6 @@
+const people = ["ram", "hari", "chandra", "narayan", "krishna"];
+const ages = [2, 12, 12, 2, 2, 2];
+
+console.log(people);
+
+module.exports = { people, ages };
